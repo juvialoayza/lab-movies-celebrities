258f81a1a868d6c9cfb5be609b869ef02d5d2bd7
Javier Palomero <jmpalomeros@gmail.com>
2022-10-18 17:57:30 +0200

    iter 6 done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,40 @@
 const router = require("express").Router();
 
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model.js")
 
+router.get("/create", async (req, res, next)=>{
+    
+    
+    try{
+        const celebritiesList = await Celebrity.find()
+        res.render("movies/new-movie.hbs",{
+            celebritiesList
+        })       
+
+    }
+    catch(error){
+        next(error)
+    }
+})
+
+router.post("/create", async (req, res, next)=>{
+
+    const{title, genre, plot, cast} = req.body
+
+    try{
+        await Movie.create({title, 
+            genre, 
+            plot, 
+            cast})
+        
+        res.redirect("/movies")
+    }
+    catch(error){
+        next(error)
+    }
+
+})
 
 
 
