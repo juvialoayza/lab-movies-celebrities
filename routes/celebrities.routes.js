d20dc7e7440d7d5a6397822f17c3e64389975638
Javier Palomero <jmpalomeros@gmail.com>
2022-10-18 16:58:45 +0200

    itera 4 done

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model.js")
+
+
+//crear celebrity
+
+router.get("/create", (req, res, next) => {
+    res.render("celebrities/new-celebrity.hbs")
+})
+
+router.post("/create", async (req, res, next) => {
+
+const {name, occupation, catchPhrase} = req.body
+
+try {
+    await Celebrity.create({
+        name,
+        occupation,
+        catchPhrase
+    })
+
+    res.redirect("/")
+
+}   catch(error){
+    next(error)
+}
+})
+
+//listado de celebrities
+
+router.get("/", async(req,res,next)=>{
+
+    try{
+        const celebritiesList =  await Celebrity.find()
+
+        res.render("celebrities/celebrities.hbs",{
+            celebritiesList
+        })
+
+    }catch(err){
+        next (err)
+    }
+    
+
+
+
+})
+
+
+
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,13 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+//traemos las rutas de celebrities y las acortamos
+const celebritiesRoutes = require("./celebrities.routes.js")
+router.use("/celebrities", celebritiesRoutes)
+
+//traemos las rutas de movies y las acortamos
+const moviesRoutes = require("./movies.routes.js")
+router.use("/movies", moviesRoutes)
+
+
 module.exports = router;
